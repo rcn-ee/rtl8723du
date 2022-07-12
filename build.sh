bad1a7f2a8018c3cc32b1d011093698436dac32b
Robert Nelson <robertcnelson@gmail.com>
2022-07-12 13:19:34 -0500

    add basic building
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+if [ -f .builddir ] ; then
+	if [ -d ./src ] ; then
+		rm -rf ./src || true
+	fi
+
+	echo "git clone https://github.com/lwfinger/rtl8723du.git ./src --depth=1"
+	git clone https://github.com/lwfinger/rtl8723du.git ./src --depth=1
+
+	if [ "x${ARCH}" = "xarmv7l" ] ; then
+		make_options="CROSS_COMPILE= KSRC=/build/buildd/linux-src"
+	else
+		x86_dir="`pwd`/../../normal"
+		if [ -f `pwd`/../../normal/.CC ] ; then
+			. `pwd`/../../normal/.CC
+			make_options="CROSS_COMPILE=${CC} KSRC=${x86_dir}/KERNEL"
+		fi
+	fi
+
+	cd ./src
+	make ARCH=arm ${make_options} clean
+	echo "make ARCH=arm ${make_options}"
+	make ARCH=arm ${make_options} all
+fi
+#
