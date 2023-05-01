835467664fa6655f23483bd595ccb655ac5fdd4d
Robert Nelson <robertcnelson@gmail.com>
2023-05-01 15:48:43 -0500

    build.sh: fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -10,14 +10,14 @@ if [ -f .builddir ] ; then
 	echo "git clone https://github.com/lwfinger/rtl8723du.git ./src --depth=1"
 	git clone https://github.com/lwfinger/rtl8723du.git ./src --depth=1
 
-	if [ "x${ARCH}" = "xarmv7l" ] ; then
-		make_options="CROSS_COMPILE= KSRC=/build/buildd/linux-src"
-	else
+	if [ "x${ARCH}" = "xx86_64" ] ; then
 		x86_dir="`pwd`/../../normal"
 		if [ -f `pwd`/../../normal/.CC ] ; then
 			. `pwd`/../../normal/.CC
 			make_options="CROSS_COMPILE=${CC} KSRC=${x86_dir}/KERNEL"
 		fi
+	else
+		make_options="CROSS_COMPILE= KSRC=/build/buildd/linux-src"
 	fi
 
 	cd ./src
